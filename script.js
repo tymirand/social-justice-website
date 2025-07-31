6d870d4eb6eae24e0ae9e8d48ab8fa059efd53e9
tymirand <215926625+tymirand@users.noreply.github.com>
2025-07-30 18:13:11 -0700

    add break pts and pages

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-const darkModeToggleButton = document.getElementById('darkModeButton')
-darkModeToggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('darkMode')
-    // if darkModeToggleButton 
+const darkModeToggleButton = document.getElementById("dark-mode-toggle");
+darkModeToggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
+const dragonReader = document.getElementById("dragon-reader");
+dragonReader.addEventListener("click", () => {
+  console.log("Clicking");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bookContainer = document.getElementById("book-container");
+  const loadingContainer = 
+
+  fetch(`https://gutendex.com/books/?topic=novels`)
+    .then(handleResponse)
+    .catch(handleError);
+
+  function handleResponse(response) {
+    if (!response.ok) {
+      throw new Error("Problem Encountered");
+    }
+    return response.json().then((data) => {
+      
+      if (data.results) {
+        console.log(data.results);
+      } else {
+        bookContainer.innerHTML =
+          "<p>No novels found. Try a different search.</p>";
+      }
+    });
+  }
 });
